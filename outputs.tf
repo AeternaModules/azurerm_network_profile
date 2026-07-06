4dfde97e5fa6f0102d3beabe2923239528c7fefd
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,28 @@ output "network_profiles" {
   description = "All network_profile resources"
   value       = azurerm_network_profile.network_profiles
 }
+output "network_profiles_container_network_interface" {
+  description = "List of container_network_interface values across all network_profiles"
+  value       = [for k, v in azurerm_network_profile.network_profiles : v.container_network_interface]
+}
+output "network_profiles_container_network_interface_ids" {
+  description = "List of container_network_interface_ids values across all network_profiles"
+  value       = [for k, v in azurerm_network_profile.network_profiles : v.container_network_interface_ids]
+}
+output "network_profiles_location" {
+  description = "List of location values across all network_profiles"
+  value       = [for k, v in azurerm_network_profile.network_profiles : v.location]
+}
+output "network_profiles_name" {
+  description = "List of name values across all network_profiles"
+  value       = [for k, v in azurerm_network_profile.network_profiles : v.name]
+}
+output "network_profiles_resource_group_name" {
+  description = "List of resource_group_name values across all network_profiles"
+  value       = [for k, v in azurerm_network_profile.network_profiles : v.resource_group_name]
+}
+output "network_profiles_tags" {
+  description = "List of tags values across all network_profiles"
+  value       = [for k, v in azurerm_network_profile.network_profiles : v.tags]
+}
 
