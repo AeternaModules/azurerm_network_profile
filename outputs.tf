1510c1a7ca6220b3b8d8049c5b09a2b75d3638ee
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:24 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "network_profiles" {
-  description = "All network_profile resources"
-  value       = azurerm_network_profile.network_profiles
-}
 output "network_profiles_container_network_interface" {
-  description = "List of container_network_interface values across all network_profiles"
-  value       = [for k, v in azurerm_network_profile.network_profiles : v.container_network_interface]
+  description = "Map of container_network_interface values across all network_profiles, keyed the same as var.network_profiles"
+  value       = { for k, v in azurerm_network_profile.network_profiles : k => v.container_network_interface }
 }
 output "network_profiles_container_network_interface_ids" {
-  description = "List of container_network_interface_ids values across all network_profiles"
-  value       = [for k, v in azurerm_network_profile.network_profiles : v.container_network_interface_ids]
+  description = "Map of container_network_interface_ids values across all network_profiles, keyed the same as var.network_profiles"
+  value       = { for k, v in azurerm_network_profile.network_profiles : k => v.container_network_interface_ids }
 }
 output "network_profiles_location" {
-  description = "List of location values across all network_profiles"
-  value       = [for k, v in azurerm_network_profile.network_profiles : v.location]
+  description = "Map of location values across all network_profiles, keyed the same as var.network_profiles"
+  value       = { for k, v in azurerm_network_profile.network_profiles : k => v.location }
 }
 output "network_profiles_name" {
-  description = "List of name values across all network_profiles"
-  value       = [for k, v in azurerm_network_profile.network_profiles : v.name]
+  description = "Map of name values across all network_profiles, keyed the same as var.network_profiles"
+  value       = { for k, v in azurerm_network_profile.network_profiles : k => v.name }
 }
 output "network_profiles_resource_group_name" {
-  description = "List of resource_group_name values across all network_profiles"
-  value       = [for k, v in azurerm_network_profile.network_profiles : v.resource_group_name]
+  description = "Map of resource_group_name values across all network_profiles, keyed the same as var.network_profiles"
+  value       = { for k, v in azurerm_network_profile.network_profiles : k => v.resource_group_name }
 }
 output "network_profiles_tags" {
-  description = "List of tags values across all network_profiles"
-  value       = [for k, v in azurerm_network_profile.network_profiles : v.tags]
+  description = "Map of tags values across all network_profiles, keyed the same as var.network_profiles"
+  value       = { for k, v in azurerm_network_profile.network_profiles : k => v.tags }
 }
 
